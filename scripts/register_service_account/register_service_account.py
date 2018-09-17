7f8484a4ca6b7d4131c7dae30842cc1ecb3d7e94
Dave Larson <dlarson@genome.wustl.edu>
2018-09-17 09:11:23 -0500

    Try using application defaults instead

--- scripts/register_service_account/register_service_account.py
+++ scripts/register_service_account/register_service_account.py
@@ -13,10 +13,12 @@ def main():
 
     args = parser.parse_args()
 
-    from oauth2client.service_account import ServiceAccountCredentials
+    import google.auth
+    #from oauth2client.service_account import ServiceAccountCredentials
     scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(args.json_credentials, scopes=scopes)
-    headers = {"Authorization": "bearer " + credentials.get_access_token().access_token}
+    #credentials = ServiceAccountCredentials.from_json_keyfile_name(args.json_credentials, scopes=scopes)
+    credentials, project_id = google.auth.default(scopes=scopes)
+    headers = {"Authorization": "bearer " + credentials.token}
     headers["User-Agent"] = firecloud_api.FISS_USER_AGENT
 
     uri = args.fc_url + "/register/profile"
