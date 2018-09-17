08c9b95d948572ec0874f1f97164e0ab2b85898a
Dave Larson <delarson@wustl.edu>
2018-09-17 14:25:38 -0500

    Remove committed out code

--- scripts/register_service_account/register_service_account.py
+++ scripts/register_service_account/register_service_account.py
@@ -7,16 +7,13 @@ def main():
     parser = ArgumentParser(description="Register a service account for use in FireCloud.")
 
     # Core application arguments
-#    parser.add_argument('-j', '--json_credentials', dest='json_credentials', action='store', required=True, help='Path to the json credentials file for this service account.')
     parser.add_argument('-e', '--owner_email', dest='owner_email', action='store', required=True, help='Email address of the person who owns this service account')
     parser.add_argument('-u', '--url', dest='fc_url', action='store', default="https://api.firecloud.org", required=False, help='Base url of FireCloud server to contact (default https://api.firecloud.org)')
 
     args = parser.parse_args()
 
     import google.auth
-    #from oauth2client.service_account import ServiceAccountCredentials
     scopes = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
-    #credentials = ServiceAccountCredentials.from_json_keyfile_name(args.json_credentials, scopes=scopes)
     credentials, project_id = google.auth.default(scopes=scopes)
     request = google.auth.transport.requests.Request()
     credentials.refresh(request)
